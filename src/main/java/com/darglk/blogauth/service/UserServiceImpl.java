943b79f19c9c118f9ea92e5e5d46dbc23d9b7c57
darglk <darglk@gmail.com>
2023-03-10 11:16:42 +0100

    added rabbit to project

--- src/main/java/com/darglk/blogauth/service/UserServiceImpl.java
+++ src/main/java/com/darglk/blogauth/service/UserServiceImpl.java
@@ -3,11 +3,14 @@ package com.darglk.blogauth.service;
 import com.darglk.blogauth.connector.KeycloakConnector;
 import com.darglk.blogauth.repository.UserRepository;
 import com.darglk.blogauth.rest.model.LoginResponse;
+import com.darglk.blogcommons.events.Subjects;
 import com.darglk.blogcommons.exception.NotFoundException;
 import com.darglk.blogcommons.model.AuthorityResponse;
 import com.darglk.blogcommons.model.LoginRequest;
 import com.darglk.blogcommons.model.UserResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,6 +22,7 @@ public class UserServiceImpl implements UsersService {
 
     private final KeycloakConnector keycloakConnector;
     private final UserRepository userRepository;
+    private final RabbitTemplate rabbitTemplate;
 
     @Override
     public LoginResponse login(LoginRequest loginRequest) {
@@ -26,7 +30,7 @@ public class UserServiceImpl implements UsersService {
         var loginResponse = new LoginResponse();
         loginResponse.setRefreshToken(kcResponse.getRefreshToken());
         loginResponse.setAccessToken(kcResponse.getAccessToken());
-
+        rabbitTemplate.convertAndSend(Subjects.UserCreated.getSubject(), "WYKURWIŚCIE");
         return loginResponse;
     }
 
@@ -48,5 +52,10 @@ public class UserServiceImpl implements UsersService {
                 .collect(Collectors.toList()));
 
         return userResponse;
+    }
+
+    @RabbitListener(queues = "user_created")
+    public void listen(String in) {
+        System.out.println(in);
     }
 }
